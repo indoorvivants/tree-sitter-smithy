0dd4f914457ebc4a0ace8177e0488f1ec2ffe31c
Benoit Louy <benoit.louy@fastmail.com>
2022-09-25 09:50:25 -0400

    update previous grammar

--- grammar.js
+++ grammar.js
@@ -8,34 +8,110 @@ module.exports = grammar({
   ],
 
   rules: {
+    // TODO: add the actual grammar rules
     source_file: $ =>
       seq(
         optional($.control_section),
         optional($.metadata_section),
-        optional($.definition_section)
+        optional($.shape_section)
       ),
 
-    control_section: $ => repeat1($.control_statement),
-    control_statement: $ => seq('$', $.node_object_key, ':', $.node_value),
+    shape_section: $ => seq(
+      $.namespace_statement,
+      repeat($._definition)
+    ),
 
-    metadata_section: $ => repeat1($.metadata_statement),
-    metadata_statement: $ => seq("metadata", $.node_object_key, '=', $.node_value),
+    _definition: $ => choice(
+      $.use_statement,
+      $.apply_trait_statement,
+      $.shape_statement
+    ),
 
-    definition_section: $ => seq($.namespace_statement, optional($.use_section), optional($.definition_statements)),
-
-    namespace_statement: $ => seq('namespace', $.namespace),
-
-    use_section: $ => repeat1($.use_statement),
     use_statement: $ => seq('use', $.absolute_root_shape_id),
-
-    definition_statements: $ => repeat1($.definition_statement),
-    definition_statement: $ => choice($.shape_statement, $.apply_statement),
-
+    namespace_statement: $ => seq('namespace', $.namespace),
+    metadata_section: $ => repeat1($.metadata_statement),
+    metadata_statement: $ => seq('metadata', $.identifier, '=', $.node_value),
+    control_section: $ => repeat1($.control_statement),
+    control_statement: $ => seq($.control_var_name, $.node_value),
     shape_statement: $ => seq(optional($.trait_statements), $.shape_body),
+    shape_body: $ => choice(
+      $.simple_shape_statement,
+      $.list_shape_statement,
+      $.map_shape_statement,
+      $.set_shape_statement,
+      $.structure_shape_statement,
+      $.union_shape_statement,
+      $.operation_shape_statement,
+      $.resource_shape_statement,
+      $.service_shape_statement,
+      $.enum_shape_statement
+    ),
 
-    apply_statement: $ => choice($.apply_statement_singular, $.apply_statement_block),
-    apply_statement_singular: $ => seq('apply', $.shape_id, $.trait),
-    apply_statement_block: $ => seq('apply', $.shape_id, '{', optional($.trait_statements), '}'),
+    absolute_root_shape_id: $ => seq($.namespace, '#', $.identifier),
+    root_shape_id: $ => choice($.absolute_root_shape_id, $.identifier),
+    shape_id_member: $ => seq('$', $.identifier),
+    shape_id: $ => prec.left(seq($.root_shape_id, repeat($.shape_id_member))),
+    control_var_name: $ => seq('$', $.node_object_key, ':'),
+    namespace: $ => seq($.identifier, repeat(seq('.', $.identifier))),
+
+    simple_shape_statement: $ => seq($.simple_type_name, $.identifier, optional($.mixins)),
+    list_shape_statement: $ => seq('list', $.identifier, optional($.mixins), $.shape_members),
+    set_shape_statement: $ => seq('set', $.identifier, optional($.mixins), $.shape_members),
+    map_shape_statement: $ => seq('map', $.identifier, optional($.mixins), $.shape_members),
+    structure_shape_statement: $ => seq(
+      'structure',
+      $.identifier,
+      optional($.structure_resource),
+      optional($.mixins),
+      $.shape_members
+    ),
+    union_shape_statement: $ => seq('union', $.identifier, optional($.mixins), $.shape_members),
+
+    service_shape_statement: $ => seq('service', $.identifier, optional($.mixins), $.node_object),
+    operation_shape_statement: $ => seq('operation', $.identifier, optional($.mixins), $.operation_body),
+    resource_shape_statement: $ => seq('resource', $.identifier, optional($.mixins), $.node_object),
+
+    enum_shape_statement: $ => seq(
+      choice('enum', 'intEnum'),
+      $.identifier,
+      optional($.mixins),
+      $.enum_members
+    ),
+    enum_members: $ => seq('{', repeat($.enum_member), '}'),
+    enum_member: $ => seq($.identifier, optional($.value_assignment)),
+
+    shape_members: $ => seq(
+      '{',
+      repeat($.shape_member),
+      '}'
+    ),
+    shape_member: $ => seq(
+      optional($.trait_statements),
+      choice(
+        $.shape_member_kvp,
+        $.shape_member_elided
+      ),
+      optional($.value_assignment)
+    ),
+    shape_member_kvp: $ => seq($.identifier, ':', $.shape_id),
+    shape_member_elided: $ => seq('$', $.identifier),
+
+    operation_body: $ => seq('{', repeat($.operation_member), '}'),
+    operation_member: $ => choice(
+      $.shape_member_kvp,
+      seq($.identifier, $.inline_structure),
+      $.operation_errors
+    ),
+    operation_errors: $ => seq($.identifier, ':', '[', repeat($.operation_error), ']'),
+    operation_error: $ => $.identifier,
+
+    inline_structure: $ => seq(
+      ':=',
+      optional($.trait_statements),
+      optional($.structure_resource),
+      optional($.mixins),
+      $.shape_members
+    ),
 
     trait_statements: $ => repeat1($.trait),
     trait: $ => seq('@', $.shape_id, optional($.trait_body)),
@@ -44,87 +120,31 @@ module.exports = grammar({
     trait_structure: $ => repeat1($.trait_structure_kvp),
     trait_structure_kvp: $ => seq($.node_object_key, ':', $.node_value),
 
-    shape_body: $ => choice(
-      $.simple_shape_statement,
-      $.enum_shape_statement,
-      $.list_statement,
-      $.map_statement,
-      $.structure_statement,
-      $.union_statement,
-      $.service_statement,
-      $.resource_statement,
-      $.operation_statement
-    ),
-
-    simple_shape_statement: $ => seq($.simple_type_name, $.identifier, optional($.mixins)),
-
-    enum_shape_statement: $ => seq($.enum_type_name, $.identifier, optional($.mixins), $.enum_members),
-    enum_type_name: $ => choice('enum', 'intEnum'),
-    enum_members: $ => seq('{', repeat($.enum_member), '}'),
-    enum_member: $ => seq($.identifier, optional($.value_assignment)),
-
-    list_statement: $ => seq($.list_type_name, $.identifier, optional($.mixins), $.shape_members),
-    list_type_name: $ => choice('list', 'set'),
-
-    map_statement: $ => seq('map', $.identifier, optional($.mixins), $.shape_members),
-
-    structure_statement: $ => seq('structure', $.identifier, optional($.structure_resource), optional($.mixins), $.shape_members_with_assignment),
-    structure_resource: $ => seq('for', $.shape_id),
-
-    union_statement: $ => seq('union', $.identifier, optional($.mixins), $.shape_members),
-
-    service_statement: $ => seq('service', $.identifier, optional($.mixins), $.node_object),
-
-    resource_statement: $ => seq('resource', $.identifier, optional($.mixins), $.node_object),
-
-    operation_statement: $ => seq('operation', $.identifier, optional($.mixins), $.operation_body),
-    operation_body: $ => seq('{', repeat($.operation_member), '}'),
-    operation_member: $ => choice(
-      $.explicit_member,
-      seq($.identifier, $.inline_structure),
-      $.operation_errors
-    ),
-    operation_errors: $ => seq($.identifier, ':', '[', repeat($.operation_error), ']'),
-    operation_error: $ => $.identifier,
-
-
-    inline_structure: $ => seq(':=', optional($.trait_statements), optional($.structure_resource), optional($.mixins), $.shape_members_with_assignment),
-
-    shape_members_with_assignment: $ => seq('{', repeat($.shape_member_with_assignment), '}'),
-    shape_member_with_assignment: $ => seq(
-      optional($.trait_statements),
-      choice($.elided_member, $.explicit_member),
-      optional($.value_assignment)
-    ),
-
-    shape_members: $ => seq('{', repeat($.shape_member), '}'),
-    shape_member: $ => seq(optional($.trait_statements), choice($.elided_member, $.explicit_member)),
-    elided_member: $ => seq('$', $.identifier),
-    explicit_member: $ => seq($.identifier, ':', $.shape_id),
-
-    value_assignment: $ => seq('=', $.node_value),
+    apply_trait_statement: $ => choice($.apply_statement_singular, $.apply_statement_block),
+    apply_statement_singular: $ => seq('apply', $.shape_id, $.trait),
+    apply_statement_block: $ => seq('apply', $.shape_id, '{', optional($.trait_statements), '}'),
 
     mixins: $ => seq('with', '[', repeat1($.shape_id), ']'),
+    structure_resource: $ => seq('for', $.shape_id),
+    value_assignment: $ => seq('=', $.node_value),
 
-    node_value: $ => choice(
-      $.node_object,
-      $.node_array,
-      $.number,
-      $.node_keywords,
-      $.node_string_value
-    ),
-    node_object: $ => seq('{', repeat($.node_object_kvp), '}'),
-    node_object_kvp: $ => seq($.node_object_key, ':', $.node_value),
+    comment: $ => seq('//', /.*/),
+    documentation_comment: $ => seq('///', /.*/),
+
+    node_value: $ => choice($.node_array, $.node_object, $.node_keywords, $.node_string_value, $.number),
     node_object_key: $ => choice($.quoted_text, $.identifier),
-    node_array: $ => seq('[', repeat($.node_value), ']'),
     node_string_value: $ => choice($.shape_id, $.text_block, $.quoted_text),
-
-    shape_id: $ => prec.left(seq($.root_shape_id, optional($.shape_id_member))),
-    root_shape_id: $ => choice($.absolute_root_shape_id, $.identifier),
-    absolute_root_shape_id: $ => seq($.namespace, '#', $.identifier),
-    namespace: $ => seq($.identifier, repeat(seq('.', $.identifier))),
-    shape_id_member: $ => seq('$', $.identifier),
-
+    node_array: $ => seq(
+      '[',
+      repeat($.node_value),
+      ']'
+    ),
+    node_object: $ => seq(
+      '{',
+      repeat($.node_object_kvp),
+      '}'
+    ),
+    node_object_kvp: $ => seq($.node_object_key, ':', $.node_value),
     node_keywords: $ => /true|false|null/,
 
     identifier: $ => /([_]*)([a-zA-Z])([a-zA-Z0-9_]*)/,
@@ -133,8 +153,5 @@ module.exports = grammar({
     simple_type_name: $ => /blob|boolean|string|byte|short|integer|long|float|double|bigInteger|timestamp|document/,
 
     number: $ => /\d+/,
-
-    comment: $ => seq('//', /.*/),
-    documentation_comment: $ => seq('///', /.*/),
   }
 });
